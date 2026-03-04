e39ee5c27a7c38fa6c01e04ab4a725f94ff4ba3f
조치호 <143821717+xxh3898@users.noreply.github.com>
2026-03-04 23:55:36 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 196 ms, Memory: 18464 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+
+        int denominator = a * e - b * d;
+
+        int x = (c * e - b * f) / denominator;
+        int y = (a * f - c * d) / denominator;
+
+        System.out.println(x + " " + y);
+    }
+}
